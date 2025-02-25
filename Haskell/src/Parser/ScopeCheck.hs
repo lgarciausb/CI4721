bb980f106813838da813b74b99366237d3f9378d
initial-mockingbird <daniel.andres.pinto@gmail.com>
2025-02-25 05:11:43 -0400

    scoping done

---
+++ Haskell/src/Parser/ScopeCheck.hs
@@ -0,0 +1,201 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Parser.ScopeCheck where
+
+import Parser.LexerDefinitions 
+import Parser.Lexer
+import Parser.Parser
+import Control.Monad.Reader 
+import Control.Monad.Writer
+import TypedMap 
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Kind 
+import Control.Monad 
+import Data.Foldable
+
+data Void1 (a :: ())
+
+type ScopeDict = TypeRepMap () Void1
+type ErrLogs = [Text]
+
+appendToLog :: MonadWriter ErrLogs m => Text -> m ()
+appendToLog = tell . pure 
+
+showGammaErrors :: GammaErrors -> Text 
+showGammaErrors e = case e of 
+  TypeMismatch s (ExpectedType et) (ActualType at) 
+    -> "Type Mismatch at symbol: " 
+    <> s 
+    <> ". Expected type: "
+    <> et 
+    <> "but instead got: "
+    <> at
+  VariableNotDefined s 
+    -> "Symbol not defined: " <> s
+  VariableAlreadyDeclared s 
+    -> "Symbol cannot be re-declared: " <> s
+  VariableNotInitialized s 
+    -> "Symbol not initialized: " <> s
+
+class Scope a where 
+  checkScoping :: (MonadReader ScopeDict m, MonadWriter ErrLogs m, MonadIO m) => a -> m ScopeDict
+
+instance Scope (RecordPattern AlexPosn) where
+  checkScoping (RecordPattern (v,t,_) xs _) = checkScoping (t : [a | (_,a,_) <- xs]) >> do 
+    env0 <- ask 
+    foldM (flip $ declareFresh @'() @Void1) env0 $ v : [a | (a,_,_) <- xs]
+   
+
+
+instance Scope (PTypes AlexPosn) where
+  checkScoping (PAtom v (AlexPn _ l c)) = do 
+    e0 <- ask 
+    let m0 = v `notInScope` e0
+    when m0 $ appendToLog 
+      ( "Scope error at line " 
+      <> T.show l 
+      <> ", column " 
+      <> T.show c
+      <> ". While parsing the atom: "
+      <> v
+      <> "; Atom not in scope."
+      )
+    pure e0
+
+  checkScoping (PId v (AlexPn _ l c)) = do 
+    e0 <- ask 
+    let m0 = v `notInScope` e0
+    when m0 $ appendToLog 
+      ( "Scope error at line " 
+      <> T.show l 
+      <> ", column " 
+      <> T.show c
+      <> ". While parsing the type: "
+      <> v
+      <> "; Type not in scope."
+      )
+    pure e0
+
+  checkScoping (PUnion a b) = checkScoping a *> checkScoping b
+  checkScoping (PRecord x) = checkScoping x
+
+instance Scope (FunArg AlexPosn) where
+  checkScoping (FunArg t v _ (AlexPn _ l c)) = do 
+    _ <- checkScoping t 
+    e0 <- ask 
+    let m0 = v `inScope` e0
+    when m0 $ appendToLog 
+      ( "Scope error at line: "
+      <> T.show l
+      <> ", column: "
+      <> T.show c
+      <> ", at the function parameter: "
+      <> v
+      <> ". Symbol already defined."
+      )
+    declareFresh @'() @Void1 v e0
+
+
+instance Scope (FunctionDef AlexPosn) where
+  checkScoping (FunctionDef t v fs as (AlexPn _ l _)) = do 
+    _ <-  checkScoping t 
+    e0 <- ask 
+    let m0 = v `inScope` e0
+    when m0 $ appendToLog 
+      ( "Scope error at line: "
+      <> T.show l
+      <> ", at the declaration of the function: "
+      <> v
+      <> ". Symbol already defined."
+      )
+    e1 <- declareFresh @'() @Void1 v e0 >>= \e0' -> local (const e0') $ checkScoping fs
+    local (const e1) $ checkScoping as
+
+instance Scope (LValuable AlexPosn) where
+  checkScoping (PLId v (AlexPn _ l c)) = ask >>= \d -> case v `inScope` d of
+    True -> pure d
+    False -> appendToLog 
+      ( "Scope error at line " 
+      <> T.show l 
+      <> ", column " 
+      <> T.show c
+      <> ". Identifier: "
+      <> v
+      <> " not defined"
+      ) *> ask
+  checkScoping (PLIndexed v xs p) = checkScoping (PLId v p) *> checkScoping xs
+  checkScoping (PLDot v l p) = checkScoping (PLId v p) *> checkScoping l
+
+instance Scope (Pattern AlexPosn) where
+  checkScoping (PaId t _ _) = ask >>= declareFresh @'() @Void1 t
+  checkScoping (PaPattern x) = checkScoping x
+  checkScoping _ = ask
+
+instance Scope (Action AlexPosn) where
+  checkScoping (For x xs as _) 
+    = checkScoping xs 
+    >> checkScoping x 
+    >>= \e -> local (const e) $ checkScoping as  
+  checkScoping (While e as _) = checkScoping e >> checkScoping as 
+  checkScoping (Assign x e) = checkScoping x >> checkScoping e 
+  checkScoping (AExpression e) = checkScoping e 
+  checkScoping (Return e _) = checkScoping e
+
+instance Scope (LoopAction AlexPosn) where 
+  checkScoping (LAction a) = checkScoping a
+  checkScoping _ = ask
+
+instance Scope (Expression AlexPosn) where
+  checkScoping (ELValuable e) = checkScoping e
+  checkScoping (New t es _) = checkScoping t >> checkScoping es
+  checkScoping (Ref x _) = checkScoping x
+  checkScoping (Neg x _) = checkScoping x
+  checkScoping (Plus a b _) = checkScoping a >> checkScoping b
+  checkScoping (Times a b _) = checkScoping a >> checkScoping b
+  checkScoping (Divide a b _) = checkScoping a >> checkScoping b
+  checkScoping (Power a b _) = checkScoping a >> checkScoping b
+  checkScoping (Mod a b _) = checkScoping a >> checkScoping b
+  checkScoping (Minus a b _) = checkScoping a >> checkScoping b
+  checkScoping (ELT a b _) = checkScoping a >> checkScoping b
+  checkScoping (EGT a b _) = checkScoping a >> checkScoping b 
+  checkScoping (NotEq a b _) = checkScoping a >> checkScoping b
+  checkScoping (EEq a b _) = checkScoping a >> checkScoping b
+  checkScoping (EGTEq a b _) = checkScoping a >> checkScoping b
+  checkScoping (ELTEq a b _) = checkScoping a >> checkScoping b 
+  checkScoping (Or a b _) = checkScoping a >> checkScoping b
+  checkScoping (And a b _) = checkScoping a >> checkScoping b 
+  checkScoping (Not a _) = checkScoping a 
+  checkScoping (Arr es _) = checkScoping es 
+  checkScoping (Match e pas _) = checkScoping e 
+    >> traverse_ (\(p,as) -> checkScoping p >>= \d -> local (const d) $ checkScoping as) pas
+    >> ask
+  checkScoping (FApp f args (AlexPn _ l c)) = ask >>= \env0 -> do 
+    when (f `notInScope` env0) $ appendToLog
+      ( "Scope error at line " 
+      <> T.show l 
+      <> ", column " 
+      <> T.show c
+      <> ". Function Identifier: "
+      <> f
+      <> " not defined"
+      )
+    declareFresh @'() @Void1 f env0 >>= \env1 -> local (const env1) $ checkScoping args
+  checkScoping _ = ask 
+
+
+instance Scope a => Scope [a] where
+  checkScoping xs = ask >>= \d -> foldM (\d' a -> local (const d') . checkScoping $ a ) d xs 
+
+instance (Scope a,Scope b) => Scope (a,b) where  
+  checkScoping (a,b) = checkScoping a >>= \d -> local (const d) $ checkScoping b
+
+runScopeIO :: [FunctionDef AlexPosn] -> IO ErrLogs
+runScopeIO xs = execWriterT $ runReaderT (checkScoping  xs) empty
---
+++ Haskell/src/TypedMap.hs
@@ -0,0 +1,188 @@
+
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ImpredicativeTypes #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeAbstractions #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+module TypedMap where
+
+
+import Data.Map (Map)
+import qualified Data.Map as M
+
+import Unsafe.Coerce
+import Type.Reflection 
+--import Control.Lens.Lens
+import Data.Proof
+
+import Debug.Trace
+import Data.List.NonEmpty ( NonEmpty((:|)) )
+import Data.Foldable1 (foldl1')
+import Control.Concurrent hiding (yield)
+import Control.Monad.IO.Class
+import Control.Monad
+import Control.Applicative hiding (yield)
+import Data.Singletons
+import Data.String (IsString(..))
+import Data.Singletons.Decide 
+import Control.Monad.Reader
+import Data.Kind (Type)
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Functor 
+
+type Symbol = Text 
+
+-------------------------------
+-- Errors
+--------------------------------
+
+data ExpectedType = ExpectedType Text  
+data ActualType = ActualType Text
+
+data GammaErrors
+  =  TypeMismatch Symbol ExpectedType ActualType 
+  |  VariableNotDefined Symbol
+  |  VariableAlreadyDeclared Symbol
+  |  VariableNotInitialized Symbol
+
+--------------------------------
+-- Lens interface
+--------------------------------
+
+type family EvalMonad (f :: a -> Type) :: Type -> Type
+
+-- Defines a way to get, set, set fresh and obtain the name of a variable
+data LensM k (f :: k -> Type) (a :: k) = LensM
+  { getL  ::  TypeRepMap k f -> (EvalMonad f) (Either GammaErrors (f a))
+  , setL  ::  TypeRepMap k f -> f a -> (EvalMonad f) (Either GammaErrors (TypeRepMap k f))
+  , declareL ::  TypeRepMap k f -> (EvalMonad f) (Either GammaErrors (TypeRepMap k f))
+  , declareFL ::  TypeRepMap k f -> (EvalMonad f) (TypeRepMap k f)
+  , varNameM :: Symbol
+  }
+
+viewM ::LensM k f a -> TypeRepMap k f -> (EvalMonad f)(Either GammaErrors (f a))
+viewM  = getL
+
+setM :: LensM k f a -> f a -> TypeRepMap k f -> (EvalMonad f) (Either GammaErrors (TypeRepMap k f))
+setM = flip . setL
+
+declareM :: LensM k f a ->  TypeRepMap k f -> (EvalMonad f) (Either GammaErrors (TypeRepMap k f))
+declareM = declareL
+
+declareMF :: LensM k f a ->  TypeRepMap k f -> (EvalMonad f) (TypeRepMap k f)
+declareMF = declareFL
+
+
+data Any k (f :: k -> Type)   where
+  MkAny :: forall {k} (a :: k) f.  (SingI a, SingKind k, Show (Demote k)) => MVar (f a) -> Any k f 
+
+newtype TypeRepMap k f = TypeRepMap (Map Symbol (Any k f))
+
+empty :: TypeRepMap k f
+empty = TypeRepMap M.empty 
+
+scope :: TypeRepMap k f -> [Symbol]
+scope (TypeRepMap m) = M.keys m 
+
+inScope :: Symbol -> TypeRepMap k f -> Bool 
+inScope s (TypeRepMap m) =  s `M.member` m 
+
+
+notInScope :: Symbol -> TypeRepMap k f -> Bool 
+notInScope s (TypeRepMap m) = not $ s `M.member` m 
+
+
+declareFresh :: forall {k} (a :: k) (f :: k -> Type) m.  
+  ( SingI a
+  , MonadIO m
+  , Show (Demote k)
+  , SingKind k
+  ) => Symbol -> TypeRepMap k f -> m (TypeRepMap k f)
+declareFresh var (TypeRepMap d) = do 
+  mv <- liftIO $ newEmptyMVar @(f a)
+  pure . TypeRepMap $ M.insert var (MkAny mv) d
+
+declare :: forall {k} (a :: k) (f :: k -> Type) m.  
+  ( SingI a
+  , MonadIO m
+  , Show (Demote k)
+  , SingKind k
+  ) => Symbol -> TypeRepMap k f -> m (Either GammaErrors (TypeRepMap k f))
+declare var (TypeRepMap d) = case M.lookup var d of 
+  Just _ -> pure . Left $ VariableAlreadyDeclared var 
+  Nothing -> do 
+    mv <- liftIO $ newEmptyMVar @(f a)
+    pure . pure . TypeRepMap $ M.insert var (MkAny mv) d
+
+insert :: forall {k} (a :: k) (f :: k -> Type) m.  
+  ( SingI a
+  , MonadIO m
+  , SDecide k
+  , Show (Demote k)
+  , SingKind k
+  ) => Symbol -> f a -> TypeRepMap k f -> m (Either GammaErrors (TypeRepMap k f))
+insert var e (TypeRepMap d) = case M.lookup var d of 
+  Nothing -> declareFresh @a var (TypeRepMap d) >>= insert var e
+  Just (MkAny  @a' mv) -> case decideEquality (sing @a') (sing @a) of
+    Nothing -> pure . Left $ TypeMismatch var (ExpectedType . T.show $ demote @a') (ActualType . T.show $ demote @a )
+    Just Refl -> const (Right . TypeRepMap $ d) <$> liftIO (modifyMVar_ mv $ pure . (const e) ) 
+
+yield ::  forall {k} (a :: k) (f :: k -> Type) m.  
+  ( SingI a
+  , MonadIO m
+  , SDecide k
+  , Show (Demote k)
+  , SingKind k
+  ) => Symbol -> TypeRepMap k f -> m (Either GammaErrors (f a))
+yield var (TypeRepMap d) = case M.lookup var d of 
+  Nothing -> pure . Left $ VariableNotDefined var
+  Just (MkAny @a' mv) -> case decideEquality (sing @a') (sing @a) of
+    Nothing   -> pure . Left $ TypeMismatch var (ExpectedType . T.show $ demote @a') (ActualType . T.show $ demote @a )
+    Just Refl -> liftIO (tryReadMVar mv) >>= \case 
+      Nothing -> pure . Left $ VariableNotInitialized var 
+      Just c  -> pure . pure $ c
+
+
+instance forall k  (a :: k) (f :: k -> Type). 
+  ( SingI a
+  , SDecide k
+  , Show (Demote k)
+  , SingKind k
+  , MonadIO (EvalMonad f)
+  ) => IsString (LensM k f a) where
+  fromString (fromString -> var) = LensM 
+    (yield @a var) 
+    (flip $ insert var) 
+    (declare @a var) 
+    (declareFresh @a var)
+    var
+
+
+mkVar :: forall k  (a :: k) (f :: k -> Type). 
+  ( SingI a
+  , SDecide k
+  , Show (Demote k)
+  , SingKind k
+  , MonadIO (EvalMonad f)
+  ) => String -> LensM k f a
+mkVar = fromString
+
